11eb6af2143171e1b9dae329505c0f1e62bf5d59
Feyza Nur Bakır <feyzanurbakirr@gmail.com>
2024-01-13 21:23:37 +0300

    Entityler eklendi, DbContext oluşturuldu

--- Presentation/HepsiApi.WebApi/Program.cs
+++ Presentation/HepsiApi.WebApi/Program.cs
@@ -1,3 +1,5 @@
+using HepsiApi.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -10,6 +12,9 @@ builder.Services.AddSwaggerGen();
 var env = builder.Environment;
 builder.Configuration.SetBasePath(env.ContentRootPath).AddJsonFile("appsettings.json", optional:false)
     .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional:true);
+
+
+builder.Services.AddPersistence(builder.Configuration);
 
 var app = builder.Build();
 
