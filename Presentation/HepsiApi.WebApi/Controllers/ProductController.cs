3772bb62145bdc66167ce8c2c3e0c9f34df315c5
Feyza Nur Bakır <feyzanurbakirr@gmail.com>
2024-02-03 22:31:09 +0300

    CQRS

---
+++ Presentation/HepsiApi.WebApi/Controllers/ProductController.cs
@@ -0,0 +1,27 @@
+﻿using HepsiApi.Application.Features.Products.Queries.GetAllProducts;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HepsiApi.WebApi.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public ProductController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllProducts()
+        {
+            var response = await _mediator.Send(new GetAllProductsQueryRequest());
+
+            return Ok(response);
+        }
+    }
+}
