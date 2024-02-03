0f081b5455e3472b03cca6221f357ea156944811
Feyza Nur Bakır <feyzanurbakirr@gmail.com>
2024-02-03 21:48:32 +0300

    CQRS

--- Presentation/HepsiApi.WebApi/Program.cs
+++ Presentation/HepsiApi.WebApi/Program.cs
@@ -1,4 +1,5 @@
 using HepsiApi.Persistence;
+using HepsiApi.Application;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,6 +16,7 @@ builder.Configuration.SetBasePath(env.ContentRootPath).AddJsonFile("appsettings.
 
 
 builder.Services.AddPersistence(builder.Configuration);
+builder.Services.AddApplication();
 
 var app = builder.Build();
 
