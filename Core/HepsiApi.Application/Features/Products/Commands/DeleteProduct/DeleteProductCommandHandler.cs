2bbffe39ed9cf19f5f9d96dae0f808ac9c9c2804
Feyza Nur Bakır <feyzanurbakirr@gmail.com>
2024-02-08 20:48:04 +0300

    CRUD işlemler yapıldı

---
+++ Core/HepsiApi.Application/Features/Products/Commands/DeleteProduct/DeleteProductCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using HepsiApi.Application.Interfaces.UnitOfWorks;
+using HepsiApi.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HepsiApi.Application.Features.Products.Commands.DeleteProduct
+{
+    public class DeleteProductCommandHandler : IRequestHandler<DeleteProductCommandRequest>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public DeleteProductCommandHandler(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public async Task Handle(DeleteProductCommandRequest request, CancellationToken cancellationToken)
+        {
+            var product = await _unitOfWork.GetReadRepository<Product>().GetAsync(x => x.Id == request.Id && !x.IsDeleted);
+            product.IsDeleted = true;
+
+            await _unitOfWork.GetWriteRepository<Product>().UpdateAsync(product);
+            await _unitOfWork.SaveAsync();
+
+        }
+    }
+}
